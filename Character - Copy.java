ef027cd37f3f87b64077a543d9f285813e78a63d
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2022-08-22 11:07:05 +0800

    Add files via upload

---
+++ Character - Copy.java
@@ -0,0 +1,24 @@
+
+    public class Character {
+        int strength;
+        int agility;
+        int intelligence;
+        String name;
+    
+        public void sayMyName() {
+            System.out.println("HELLO I AM" + name);
+        }
+
+        public void sayMYSTRENGTH() {
+            System.out.println("MY STRENGTH IS:" + strength);
+        }
+
+        public void sayMYSINTELLIGENCE() {
+            System.out.println("MY INTELLIGENCE IS:" + intelligence + "\n");
+        }
+
+        public void sayMYAGILITY() {
+            System.out.println("MY AGILITY IS:" + agility );
+        }
+    }
+
